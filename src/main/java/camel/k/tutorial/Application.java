2816180ef30a9f146870bb8d12135e99e6eda95e
Tristan Arts <80721880+Drystan-Furor@users.noreply.github.com>
2023-11-06 16:00:37 +0100

    Setting up the Camel Servlet

--- src/main/java/camel/k/tutorial/Application.java
+++ src/main/java/camel/k/tutorial/Application.java
@@ -1,22 +1,29 @@
 @SpringBootApplication
 @ComponentScan(basePackages="com.baeldung.camel")
-@Value("${baeldung.api.path}")
-String contextPath;
-
-@Bean
-ServletRegistrationBean servletRegistrationBean() {
-		ServletRegistrationBean servlet = new ServletRegistrationBean
-		(new CamelHttpTransportServlet(), contextPath+"/*");
-		servlet.setName("CamelServlet");
-		return servlet;
-		}
 
 public class Application {
+
+	@Value("${baeldung.api.path}")
+	String contextPath;
+
+	@Bean
+	ServletRegistrationBean servletRegistrationBean() {
+		ServletRegistrationBean servlet = new ServletRegistrationBean
+				(new CamelHttpTransportServlet(), contextPath+"/*");
+		servlet.setName("CamelServlet");
+		return servlet;
+	}
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
 }
 
-
+/*
+With this structure, you declare contextPath as a member variable of the Application class,
+which will be populated with the value from the application.yml when the application starts.
+The servletRegistrationBean method defines a bean that is part of the application context,
+and it can make use of the contextPath variable because it's within the same class.
+ */
 
