90e9493bc036079124041a90df67faefa1a58d0e
Tristan Arts <80721880+Drystan-Furor@users.noreply.github.com>
2023-11-06 15:54:50 +0100

    Setting up the Camel Servlet

--- src/main/java/camel/k/tutorial/Application.java
+++ src/main/java/camel/k/tutorial/Application.java
@@ -1,7 +1,22 @@
 @SpringBootApplication
 @ComponentScan(basePackages="com.baeldung.camel")
+@Value("${baeldung.api.path}")
+String contextPath;
+
+@Bean
+ServletRegistrationBean servletRegistrationBean() {
+		ServletRegistrationBean servlet = new ServletRegistrationBean
+		(new CamelHttpTransportServlet(), contextPath+"/*");
+		servlet.setName("CamelServlet");
+		return servlet;
+		}
+
 public class Application {
+
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
 }
+
+
+
