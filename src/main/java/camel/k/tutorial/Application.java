3dd60a741c9038c9d20d11830c734ee398c7b6d6
Tristan Arts <80721880+Drystan-Furor@users.noreply.github.com>
2023-11-06 15:52:05 +0100

    main class and Camel Configurations for Spring Boot

---
+++ src/main/java/camel/k/tutorial/Application.java
@@ -0,0 +1,7 @@
+@SpringBootApplication
+@ComponentScan(basePackages="com.baeldung.camel")
+public class Application {
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+}
