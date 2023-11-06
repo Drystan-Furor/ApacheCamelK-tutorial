a8586de76bcc556dce5fee0133df67578e9b87a4
Tristan Arts <80721880+Drystan-Furor@users.noreply.github.com>
2023-11-06 15:41:14 +0100

    spring initializer with camel, initial commit

---
+++ src/test/java/camel/k/tutorial/CamelApplicationTests.java
@@ -0,0 +1,13 @@
+package camel.k.tutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CamelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
